c3cbdb8ff3da3485b332ec1bb4aee0e24cc8b411
Filipe Bicho <fb@refusion.com>
2020-05-06 12:53:28 +0200

    create start game button

--- app/src/main/java/com/filipebicho/poker/MainActivity.java
+++ app/src/main/java/com/filipebicho/poker/MainActivity.java
@@ -4,19 +4,28 @@ package com.filipebicho.poker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
 
 /**
  * Main activity class
  */
 public class MainActivity extends AppCompatActivity {
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button startButton = (Button) findViewById(R.id.start_button);
+        Animation startButtonAnimation = AnimationUtils.loadAnimation(this, R.anim.start_button);
 
+        startButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startButton.startAnimation(startButtonAnimation);
+            }
+        });
 
     }
 }

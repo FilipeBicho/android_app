fcbc727cfb4970d2d515f8d0d0808234c1f0dd12
Filipe Bicho <fb@refusion.com>
2020-05-06 14:14:32 +0200

    create game activity

---
+++ app/src/main/java/com/filipebicho/poker/GameActivity.java
@@ -0,0 +1,16 @@
+package com.filipebicho.poker;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Game Activity class
+ */
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
--- app/src/main/java/com/filipebicho/poker/MainActivity.java
+++ app/src/main/java/com/filipebicho/poker/MainActivity.java
@@ -3,8 +3,8 @@ package com.filipebicho.poker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
@@ -18,13 +18,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button startButton = (Button) findViewById(R.id.start_button);
+        Button startButton = findViewById(R.id.start_button);
         Animation startButtonAnimation = AnimationUtils.loadAnimation(this, R.anim.start_button);
 
-        startButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                startButton.startAnimation(startButtonAnimation);
-            }
+        startButton.setOnClickListener(v -> {
+            startButton.startAnimation(startButtonAnimation);
+            startActivity(new Intent(MainActivity.this, GameActivity.class));
         });
 
     }

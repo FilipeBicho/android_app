cb5533219fcc8bc6e4cae944b622f8d7241a0988
Filipe Bicho <fb@refusion.com>
2020-05-07 18:58:44 +0200

    set game activity with fullscreen

--- app/src/main/java/com/filipebicho/poker/GameActivity.java
+++ app/src/main/java/com/filipebicho/poker/GameActivity.java
@@ -1,16 +1,35 @@
 package com.filipebicho.poker;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 /**
  * Game Activity class
  */
 public class GameActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setFullScreen();
         setContentView(R.layout.activity_game);
+
     }
+
+    /**
+     * Set Activity with fullscreen
+     */
+    private void setFullScreen()
+    {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+    }
+
 }
